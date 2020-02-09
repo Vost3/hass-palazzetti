e50c1e247758ba55e0a0dbb73a474e3196e6e81b
BoubourSe <svsoft@free.fr>
2020-02-09 10:01:43 +0100

    (fix): PEP8

--- custom_components/palazzetti/__init__.py
+++ custom_components/palazzetti/__init__.py
@@ -12,6 +12,7 @@ from datetime import timedelta
 from homeassistant.helpers.event import async_track_time_interval
 import homeassistant.helpers.config_validation as cv
 
+import time
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -31,22 +32,21 @@ async def async_setup(hass, config):
     _LOGGER.debug("Init of palazzetti component")
 
     hass.data[DOMAIN] = Palazzetti(hass, config)
-        
+
 
     # loop for update state of stove
-    def update_datas(event_time):        
-        asyncio.run_coroutine_threadsafe( hass.data[DOMAIN].refreshMainDatas(), hass.loop)
+    def update_datas(event_time):
+        asyncio.run_coroutine_threadsafe( hass.data[DOMAIN].refresh_main_datas(), hass.loop)
 
     async_track_time_interval(hass, update_datas, INTERVAL)
-    
+
     # services
-    def setParameters(call):        
+    def set_parameters(call):
         """Handle the service call 'set'"""
-        hass.data[DOMAIN].setParameters(call.data)
+        hass.data[DOMAIN].set_parameters(call.data)
 
-    hass.services.async_register(DOMAIN, 'set_parms', setParameters)    
+    hass.services.async_register(DOMAIN, 'set_parms', set_parameters)
 
-        
     # Return boolean to indicate that initialization was successfully.
     return True
 
@@ -54,20 +54,23 @@ async def async_setup(hass, config):
 # class based on NINA 6kw
 # contact me if you have a other model of stove
 class Palazzetti(object):
-    
+
     pinged = False
     op = None
-    responseJson = None 
+    response_json = None
+
+    last_op = None
+    last_params = None
 
     """docstring for Palazzetti"""
     def __init__(self, hass, config):
 
         self.hass   = hass
         self.ip     = config[DOMAIN].get('ip', None)
-        
+
         _LOGGER.debug('Init of class palazzetti')
 
-        self.codeStatus = {
+        self.code_status = {
             0 : "OFF",
             1 : "OFF TIMER",
             2 : "TESTFIRE",
@@ -91,37 +94,37 @@ class Palazzetti(object):
             253 : "NOPELLET ALARM"
         }
 
-        self.codeFanNina = {
-            0 : "off",            
+        self.code_fan_nina = {
+            0 : "off",
             6 : "high",
-            7 : "auto"            
+            7 : "auto"
         }
-        self.codeFanNinaReversed = {
-            "off" : 0,            
+        self.code_fan_nina_reversed = {
+            "off" : 0,
             "high" : 6,
-            "auto" : 7            
+            "auto" : 7
         }
 
-        
+
         # States are in the format DOMAIN.OBJECT_ID.
-        #hass.states.async_set('palazzetti.ip', self.ip)    
+        #hass.states.async_set('palazzetti.ip', self.ip)
 
     # get main data needed
-    async def refreshMainDatas(self):
-        await self.getAlls()
-        await self.getCNTR()
+    async def refresh_main_datas(self):
+        await self.get_alls()
+        await self.get_cntr()
 
-    async def getAlls(self):        
+    async def get_alls(self):
         """Get All data or almost ;)"""
         self.op = 'GET ALLS'
-        await self.getRequest()
+        await self.get_request()
 
-    async def getCNTR(self):
+    async def get_cntr(self):
         """Get counters"""
         self.op = 'GET CNTR'
-        await self.getRequest()            
+        await self.get_request()
 
-    async def getRequest(self):
+    async def get_request(self):
 
         # params for GET
         params = (
@@ -129,38 +132,38 @@ class Palazzetti(object):
         )
 
         # request the stove
-        response = await self.asyncRequestStove(params)
+        response = await self.async_request_stove(params)
 
         if response == False:
-            _LOGGER.debug('getRequest() response false for op ' + self.op)
+            _LOGGER.debug('get_request() response false for op ' + self.op)
             return False
 
         # save response in json object
-        responseJson = json.loads(response.text)
+        response_json = json.loads(response.text)
 
         #If no response return
-        if( responseJson['SUCCESS'] != True ):
-            self.hass.states.async_set('palazzetti.stove', 'com error', self.responseJson)
+        if response_json['SUCCESS'] != True :
+            self.hass.states.async_set('palazzetti.stove', 'com error', self.response_json)
             _LOGGER.error('Error returned by CBox')
             return False
 
         # merge response with existing dict
-        if self.responseJson != None :
-            responseJsonCpy = self.responseJson.copy()
-            responseJsonCpy.update(responseJson['DATA'])
-            self.responseJson = responseJsonCpy
+        if self.response_json != None :
+            response_merged = self.response_json.copy()
+            response_merged.update(response_json['DATA'])
+            self.response_json = response_merged
         else:
-            self.responseJson = responseJson['DATA']
-            
-        self.hass.states.async_set('palazzetti.stove', 'online', self.responseJson)        
-        await self.changeStates()        
+            self.response_json = response_json['DATA']
+
+        self.hass.states.async_set('palazzetti.stove', 'online', self.response_json)
+        await self.change_states()
 
     # send request to stove
-    async def asyncRequestStove(self, params):
+    async def async_request_stove(self, params):
         _LOGGER.debug('request stove ' + self.op)
 
         if self.op is None:
-            return False        
+            return False
 
         queryStr = 'http://'+self.ip+'/cgi-bin/sendmsg.lua'
 
@@ -175,168 +178,198 @@ class Palazzetti(object):
             return False
         except requests.exceptions.ConnectTimeout:
             # equivalent of ping
-            _LOGGER.error('Please check parm ip : ' + self.ip)            
+            _LOGGER.error('Please check parm ip : ' + self.ip)
             self.hass.states.async_set('palazzetti.stove', 'offline')
             return False
-        
+
         return response
 
     # send request to stove
-    def requestStove(self, params):
-        _LOGGER.debug('request stove ' + self.op)
+    def request_stove(self, op, params):
+        _LOGGER.debug('request stove ' + op)
 
-        if self.op is None:
-            return False        
+        if op is None:
+            return False       
 
-        queryStr = 'http://'+self.ip+'/cgi-bin/sendmsg.lua'
+        # save
+        self.last_op = op
+        self.last_params = str(params)
 
-        # let's go baby
-        try:
-            response = requests.get(queryStr, params=params, timeout=30)
-        except requests.exceptions.ReadTimeout:
-            # timeout ( can happend when wifi is used )
-            _LOGGER.error('Timeout reach for request : ' + queryStr)
-            _LOGGER.info('Please check if you can ping : ' + self.ip)
-            self.hass.states.set('palazzetti.stove', 'offline')
-            return False
-        except requests.exceptions.ConnectTimeout:
-            # equivalent of ping
-            _LOGGER.error('Please check parm ip : ' + self.ip)            
-            self.hass.states.set('palazzetti.stove', 'offline')
-            return False
-        
-        if response == False:            
-            return False
+        queryStr = 'http://'+self.ip+'/cgi-bin/sendmsg.lua'        
 
-        # save response in json object
-        responseJson = json.loads(response.text)
-
+        retry = 0
+        success = False
         # error returned by Cbox
-        if( responseJson['SUCCESS'] != True ):
-            self.hass.states.async_set('palazzetti.stove', 'com error', self.responseJson)                        
-            _LOGGER.error('Error returned by CBox')
-            return False
+        while not success :
+            # let's go baby
+            try:
+                response = requests.get(queryStr, params=params, timeout=30)
+            except requests.exceptions.ReadTimeout:
+                # timeout ( can happend when wifi is used )
+                _LOGGER.error('Timeout reach for request : ' + queryStr)
+                _LOGGER.info('Please check if you can ping : ' + self.ip)
+                self.hass.states.set('palazzetti.stove', 'offline')
+                return False
+            except requests.exceptions.ConnectTimeout:
+                # equivalent of ping
+                _LOGGER.error('Please check parm ip : ' + self.ip)
+                self.hass.states.set('palazzetti.stove', 'offline')
+                return False
+
+            if response == False:
+                return False
+
+            # save response in json object
+            response_json = json.loads(response.text)
+            success = response_json['SUCCESS']
+
+            # cbox return error
+            if not success:
+                self.hass.states.async_set('palazzetti.stove', 'com error', self.response_json)
+                _LOGGER.error('Error returned by CBox - retry in 2 seconds (' +op+')')
+                time.sleep(2)
+                retry = retry + 1
+
+                if retry == 3 :
+                     _LOGGER.error('Error returned by CBox - stop retry after 3 attempt (' +op+')')
+                     break
+            
+            
 
         # merge response with existing dict
-        if self.responseJson != None :
-            responseJsonCpy = self.responseJson.copy()
-            responseJsonCpy.update(responseJson['DATA'])
-            self.responseJson = responseJsonCpy
+        if self.response_json != None :
+            response_merged = self.response_json.copy()
+            response_merged.update(response_json['DATA'])
+            self.response_json = response_merged
         else:
-            self.responseJson = responseJson['DATA']
-            
-        self.hass.states.async_set('palazzetti.stove', 'online', self.responseJson)  
+            self.response_json = response_json['DATA']
+
+        self.hass.states.async_set('palazzetti.stove', 'online', self.response_json)
 
         return response
-            
-    async def changeStates(self):
+
+    async def change_states(self):
         """Change states following result of request"""
-        if self.op == 'GET ALLS':       
-            self.hass.states.async_set('palazzetti.STATUS', self.codeStatus.get(self.responseJson['STATUS'], self.responseJson['STATUS']))    
-            self.hass.states.async_set('palazzetti.F2L', int(self.responseJson['F2L']))
-            self.hass.states.async_set('palazzetti.PWR', self.responseJson['PWR'])
-            self.hass.states.async_set('palazzetti.SETP', self.responseJson['SETP'])
-    
-    def getSEPT(self):
+        if self.op == 'GET ALLS':
+            self.hass.states.async_set('palazzetti.STATUS', self.code_status.get(self.response_json['STATUS'], self.response_json['STATUS']))
+            self.hass.states.async_set('palazzetti.F2L', int(self.response_json['F2L']))
+            self.hass.states.async_set('palazzetti.PWR', self.response_json['PWR'])
+            self.hass.states.async_set('palazzetti.SETP', self.response_json['SETP'])
+
+    def get_sept(self):
         """Get target temperature for climate"""
-        if self.responseJson == None or self.responseJson['SETP'] == None:
+        if self.response_json == None or self.response_json['SETP'] == None:
             return 0
 
-        return self.responseJson['SETP']
+        return self.response_json['SETP']
 
-    def setParameters(self, datas):
+    def set_parameters(self, datas):
         """set parameters following service call"""
-        self.setSEPT(datas.get('SETP', None))       # temperature
-        self.setPOWR(datas.get('PWR', None))        # fire power
-        self.setRFAN(datas.get('RFAN', None))       # Fan
-        self.setStatus(datas.get('STATUS', None))   # status        
-        
-    def setSEPT(self, value):
+        self.set_sept(datas.get('SETP', None))       # temperature
+        self.set_powr(datas.get('PWR', None))        # fire power
+        self.set_rfan(datas.get('RFAN', None))       # Fan
+        self.set_status(datas.get('STATUS', None))   # status
+
+    def set_sept(self, value):
         """Set target temperature"""
-        
+
         if value == None or type(value) != int:
             return
-                    
-        self.op = 'SET SETP'
+
+        # avoid multiple request
+        if op == self.last_op and str(params) == self.last_params :
+            _LOGGER.debug('retry for op :' +op+' avoided')
+            return
+
+        op = 'SET SETP'
 
         # params for GET
         params = (
-            ('cmd', self.op + ' ' + str(value)),
+            ('cmd', op + ' ' + str(value)),
         )
 
         # request the stove
-        if self.requestStove(params) == False:            
+        if self.request_stove(op, params) == False:
             return
-        
-        # change state        
-        self.hass.states.set('palazzetti.SETP', self.responseJson['SETP'])      
 
-    def setPOWR(self, value):
+        # change state
+        self.hass.states.set('palazzetti.SETP', self.response_json['SETP'])
+
+    def set_powr(self, value):
         """Set power of fire"""
-        return
-        if value == None or type(value) != int:
+        if value is None :
             return
-                    
-        self.op = 'SET POWR'
+		
+		# avoid multiple request
+        if op == self.last_op and str(params) == self.last_params :
+            _LOGGER.debug('retry for op :' +op+' avoided')
+            return
+
+        op = 'SET POWR'
 
         # params for GET
         params = (
-            ('cmd', self.op + ' ' + str(value)),
+            ('cmd', op + ' ' + str(value)),
         )
 
         # request the stove
-        if self.requestStove(params) == False:            
+        if self.request_stove(op, params) == False:
             return
-        
-        # change state        
-        self.hass.states.set('palazzetti.PWR', self.responseJson['PWR'])     
-        
-    def setRFAN(self, value):
-        """Set fan level"""            
+
+        # change state
+        self.hass.states.set('palazzetti.PWR', self.response_json['PWR'])
+
+    def set_rfan(self, value):
+        """Set fan level"""
+
         if value == None:
             return
-                
+
         # must be str or int
         if type(value) != str and type(value) != int:
             return
-                
-        self.op = 'SET RFAN'
+
+       	# avoid multiple request
+        if op == self.last_op and str(params) == self.last_params :
+            _LOGGER.debug('retry for op :' +op+' avoided')
+            return
+            
+        op = 'SET RFAN'
 
         # params for GET
         params = (
-            ('cmd', self.op + ' ' + str(value)),
+            ('cmd', op + ' ' + str(value)),
         )
 
         # request the stove
-        if self.requestStove(params) == False:            
+        if self.request_stove(op, params) == False:
             return
-        
-        # change state        
-        self.hass.states.async_set('palazzetti.F2L', self.responseJson['F2L'])
 
-    def setStatus(self, value):        
-        """start or stop stove"""        
+        # change state
+        self.hass.states.async_set('palazzetti.F2L', self.response_json['F2L'])
+
+    def set_status(self, value):
+        """start or stop stove"""
         if value == None or type(value) != str :
             return
 
         # only ON of OFF value allowed
         if value != 'on' and value != 'off':
             return
-        
-        self.op = 'CMD'
+
+        op = 'CMD'
 
         # params for GET
         params = (
-            ('cmd', self.op + ' ' + str(value)),
+            ('cmd', op + ' ' + str(value)),
         )
-                
-        # request the stove        
-        if self.requestStove(params) == False:            
+
+        # request the stove
+        if self.request_stove(op, params) == False:
             return
-        
-        # change state        
-        self.hass.states.async_set('palazzetti.STATUS', self.codeStatus.get(self.responseJson['STATUS'], self.responseJson['STATUS']))        
-                
+
+        # change state
+        self.hass.states.async_set('palazzetti.STATUS', self.code_status.get(self.response_json['STATUS'], self.response_json['STATUS']))
+
     def get_datas(self):
-        return self.responseJson
-            
+        return self.response_json
